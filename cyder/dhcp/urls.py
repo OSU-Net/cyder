285aa7dcb9222575ec60584f3aa1718f5340cd7a
Ian Fridge <ian.fridge@oregonstate.edu>
2012-10-11 13:41:44 -0700

    fixed margins in urls

--- cyder/dhcp/urls.py
+++ cyder/dhcp/urls.py
@@ -4,13 +4,13 @@ from django.views.generic.list_detail import object_detail, object_list
 from misc.generic_views import create_object, update_object, delete_object, gen_mod_dict, gen_info_dict, gen_del_dict
 
 urlpatterns = patterns('cyder.dhcp',
-                       url(r'^show/$', 'views.showall', name='dhcp-list'),
-                       url(r'^showfile/(.*)[/]$',
-                           'views.showfile', name='dhcp-show-file'),
-                       url(r'^override/(.*)[/]$', 'views.override_file',
-                           name='dhcp-show-override'),
-                       url(r'^new/$', 'views.new'),
-                       url(r'^edit/(.*)[/]$', 'views.edit'),
-                       url(r'^edit/$', 'views.create'),
-                       url(r'^delete/(.*)/$', 'views.delete'),
-                       )
+   url(r'^show/$', 'views.showall', name='dhcp-list'),
+   url(r'^showfile/(.*)[/]$',
+       'views.showfile', name='dhcp-show-file'),
+   url(r'^override/(.*)[/]$', 'views.override_file',
+       name='dhcp-show-override'),
+   url(r'^new/$', 'views.new'),
+   url(r'^edit/(.*)[/]$', 'views.edit'),
+   url(r'^edit/$', 'views.create'),
+   url(r'^delete/(.*)/$', 'views.delete'),
+)
