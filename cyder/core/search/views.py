a2fc31410f4d76e625cd52295cf9786c93bc63b3
Kevin Ngo <ktngo09@gmail.com>
2012-12-20 00:28:22 -0800

    refactor search frontend

--- cyder/core/search/views.py
+++ cyder/core/search/views.py
@@ -22,31 +22,33 @@ def search(request):
         query = parse(search)
         (addrs, cnames, domains, intrs, mxs,
          nss, ptrs, srvs, txts, misc) = compile_search(query)
-        meta = {
-            'counts': {
-                'addr': addrs.count() if addrs else 0,
-                'cname': cnames.count() if cnames else 0,
-                'domain': domains.count() if domains else 0,
-                'intr': intrs.count() if intrs else 0,
-                'mx': mxs.count() if mxs else 0,
-                'ns': nss.count() if nss else 0,
-                'ptr': ptrs.count() if ptrs else 0,
-                'txt': txts.count() if txts else 0,
-            }
-        }
+        meta = [
+            (addrs.count() if addrs else 0, 'address', 'Address Records'),
+            (cnames.count() if cnames else 0, 'cname', 'CNAMEs'),
+            (domains.count() if domains else 0, 'domain', 'Domains'),
+            (intrs.count() if intrs else 0, 'interface', 'Interfaces'),
+            (misc.count() if misc else 0, 'misc', 'Misc. Results'),
+            (mxs.count() if mxs else 0, 'mx', 'MXs'),
+            (nss.count() if nss else 0, 'nameserver', 'Nameservers'),
+            (ptrs.count() if ptrs else 0, 'ptr', 'PTRs'),
+            (srvs.count() if srvs else 0, 'srv', 'SRVs'),
+            (txts.count() if txts else 0, 'txt', 'TXTs'),
+        ]
 
         context_dict.update(**{
-          'meta': meta,
-          'address_table': tablefy(addrs, views=True),
-          'cname_table': tablefy(cnames, views=True),
-          'domain_table': tablefy(domains, views=True),
-          'intr_table': tablefy(intrs, views=True),
-          'misc_table': tablefy(misc),
-          'mx_table': tablefy(mxs, views=True),
-          'ns_table': tablefy(nss, views=True),
-          'ptr_table': tablefy(ptrs, views=True),
-          'srv_table': tablefy(srvs, views=True),
-          'txt_table': tablefy(txts, views=True),
+            'meta': meta,
+            'tables': [
+                (tablefy(addrs, views=True), 'address', 'Address Records'),
+                (tablefy(cnames, views=True), 'cname', 'CNAMEs'),
+                (tablefy(domains, views=True), 'domain', 'Domains'),
+                (tablefy(intrs, views=True), 'interface', 'Interfaces'),
+                (tablefy(misc), 'misc', 'Misc. Results'),
+                (tablefy(mxs, views=True), 'mx', 'MXs'),
+                (tablefy(nss, views=True), 'nameserver', 'Nameservers'),
+                (tablefy(ptrs, views=True), 'ptr', 'PTRs'),
+                (tablefy(srvs, views=True), 'srv', 'SRVs'),
+                (tablefy(txts, views=True), 'txt', 'TXTs'),
+            ]
         })
 
     return render(request, 'search/search.html', context_dict)
