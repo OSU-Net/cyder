6aaa47c308e9aefb52157d1901c5ac43d529f990
Kevin Ngo <ktngo09@gmail.com>
2013-01-20 02:26:05 -0800

    initial unlaming of search

--- cyder/search/urls.py
+++ cyder/search/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.simple import direct_to_template
 
 from search.views import search_ajax, search_dns_text, get_zones_json
 from search.views import search
 
 urlpatterns = patterns('',
-    url(r'^search_ajax', csrf_exempt(search_ajax)),
-    url(r'^search_dns_text', csrf_exempt(search_dns_text)),
-    url(r'^get_zones_json', csrf_exempt(get_zones_json)),
-    url(r'^$', csrf_exempt(search), name='search'),
+    url(r'^search_ajax', search_ajax),
+    url(r'^search_dns_text', search_dns_text),
+    url(r'^get_zones_json', get_zones_json),
+    url(r'^help/$', direct_to_template, {'template':
+'search/search_help.html'}, name='search-help'),
+    url(r'^$', search, name='search'),
 )
