4b12fe0c8913d2e53dadcbd2feb0c82d18f1a54e
fridgei <enkie1985@gmail.com>
2013-03-04 13:36:26 -0800

    Fixed more urls
    Removed old routes for model creation

--- cyder/cydhcp/vrf/urls.py
+++ cyder/cydhcp/vrf/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls.defaults import url, patterns
 # NEED TO FIX
 urlpatterns = patterns(
     '',
-    url(r'^create/$', VrfCreateView.as_view(),
-        name='vrf-create'),
     url(r'^(?P<vrf_pk>[\w-]+)/$', vrf_detail, name='vrf-detail'),
 ) + cydhcp_urls('vrf')
