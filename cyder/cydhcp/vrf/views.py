d1b7f0c18dfe8a75ba0afeaac818eeda28bd7026
fridgei <enkie1985@gmail.com>
2013-03-07 14:10:31 -0800

    Removed old views and added a generic detail view

--- cyder/cydhcp/vrf/views.py
+++ cyder/cydhcp/vrf/views.py
@@ -1,26 +1,14 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404,  render
 
-from cyder.cydhcp.views import CydhcpCreateView
-from cyder.cydhcp.vrf.forms import VrfForm
 from cyder.cydhcp.vrf.models import Vrf
-from cyder.base.utils import tablefy
-
-class VrfView(object):
-    model = Vrf
-    queryset = Vrf.objects.all()
-    form_class = VrfForm
-
-
-class VrfCreateView(VrfView, CydhcpCreateView):
-    """"""
 
 
 def vrf_detail(request, vrf_pk):
     vrf = get_object_or_404(Vrf, pk=vrf_pk)
-    attrs = VrfKeyValue.objects.filter(vrf=vrf)
+    attrs = vrf.vrfkeyvalue_set.all()
     return render(request, 'vrf/vrf_detail.html',
         {
-            'vrf': tablefy([vrf], views=True),
-            'attrs': tablefy([attrs], views=True),
+            'vrf': vrf,
+            'attrs': attrs,
         }
     )
