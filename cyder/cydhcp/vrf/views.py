db746960e131d78b3c086dba91cd12d5c181fdbf
Kevin Ngo <me@ngokevin.com>
2013-03-10 19:51:33 -0700

    clean vrf detail view

--- cyder/cydhcp/vrf/views.py
+++ cyder/cydhcp/vrf/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import get_object_or_404,  render
 
+from cyder.base.utils import tablefy
 from cyder.cydhcp.vrf.models import Vrf
 
 
 def vrf_detail(request, vrf_pk):
     vrf = get_object_or_404(Vrf, pk=vrf_pk)
     attrs = vrf.vrfkeyvalue_set.all()
-    return render(request, 'vrf/vrf_detail.html',
-        {
-            'vrf': vrf,
-            'attrs': attrs,
-        }
-    )
+
+    return render(request, 'vrf/vrf_detail.html', {
+        'vrf': vrf,
+        'vrf_table': tablefy([vrf]),
+        'attrs_table': tablefy(attrs),
+    })
