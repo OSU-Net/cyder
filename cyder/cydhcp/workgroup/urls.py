6ba70aab3a4efa36100e3485ed6003b46d7e672d
Kevin Ngo <ngoke@mozilla.com>
2013-03-23 13:51:34 -0700

    fix workgroup urls

--- cyder/cydhcp/workgroup/urls.py
+++ cyder/cydhcp/workgroup/urls.py
@@ -1,11 +1,4 @@
-from django.conf.urls.defaults import patterns, url
-
 from cyder.cydhcp.urls import cydhcp_urls
-from cyder.cydhcp.workgroup.views import *
 
 
 urlpatterns = cydhcp_urls('workgroup')
-urlpatterns += patterns('',
-    url(r'^(?P<pk>[\w-]+)/$', WorkgroupDetailView.as_view(),
-        name='workgroup-detail'),
-)
