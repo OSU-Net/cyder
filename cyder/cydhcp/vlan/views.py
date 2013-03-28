6f66b9da0b1faf2fa061e89aa6118a1c637475fd
fridgei <enkie1985@gmail.com>
2013-03-28 15:38:34 -0700

    Cleand up vlan and vrf detail views
    Vrf detail view now lists interfaces associted with that vrf.  I plan on doing
    the same thing with the workgroup detail view once its done.

--- cyder/cydhcp/vlan/views.py
+++ cyder/cydhcp/vlan/views.py
@@ -7,8 +7,12 @@ from cyder.cydhcp.vlan.models import Vlan
 def vlan_detail(request, vlan_pk):
     vlan = get_object_or_404(Vlan, pk=vlan_pk)
     network_paginator = make_paginator(request, vlan.network_set.all())
+    # instead of hard coding obj type we could use objet._meta.db_table in the
+    # templates I think.  I didn't do that because I was worried there woudl be
+    # edge cases I wasn't considering.
     return render(request, "vlan/vlan_detail.html", {
         "object": vlan,
+        'obj_type': 'vlan',
         "vlan_table": tablefy((vlan,)),
         "attrs_table": tablefy(vlan.vlankeyvalue_set.all()),
         "network_page_obj": network_paginator,
--- cyder/cydhcp/vrf/views.py
+++ cyder/cydhcp/vrf/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import get_object_or_404,  render
 
-from cyder.base.utils import tablefy
+from cyder.base.utils import tablefy, make_paginator
 from cyder.cydhcp.vrf.models import Vrf
 
 
 def vrf_detail(request, vrf_pk):
     vrf = get_object_or_404(Vrf, pk=vrf_pk)
     attrs = vrf.vrfkeyvalue_set.all()
-
+    static_hosts_paginator = make_paginator(
+        request, vrf.staticinterface_set.all(), record_type='static')
+    dynamic_hosts_paginator = make_paginator(
+        request, vrf.dynamicinterface_set.all(), record_type='dynamic')
     return render(request, 'vrf/vrf_detail.html', {
-        'vrf': vrf,
+        'object': vrf,
+        'obj_type': 'vrf',
         'vrf_table': tablefy([vrf]),
+        'dynamic_hosts_page_obj': dynamic_hosts_paginator,
+        'dynamic_hosts_table': tablefy(dynamic_hosts_paginator),
+        'static_hosts_page_obj': static_hosts_paginator,
+        'static_hosts_table': tablefy(static_hosts_paginator),
         'attrs_table': tablefy(attrs),
     })
