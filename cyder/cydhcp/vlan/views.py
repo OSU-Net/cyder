954b85d9e811e96c5dc297475a038d8cbf30f260
fridgei <enkie1985@gmail.com>
2013-03-28 15:20:14 -0700

    Fixed vlan detail view
    Added section to display networks associted with the given vlan

--- cyder/cydhcp/vlan/views.py
+++ cyder/cydhcp/vlan/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 
+from cyder.base.utils import make_paginator, tablefy
 from cyder.cydhcp.vlan.models import Vlan
 
 
 def vlan_detail(request, vlan_pk):
     vlan = get_object_or_404(Vlan, pk=vlan_pk)
-    attrs = vlan.vlankeyvalue_set.all()
+    network_paginator = make_paginator(request, vlan.network_set.all())
     return render(request, "vlan/vlan_detail.html", {
-        "vlan": vlan,
-        "attrs": attrs
+        "object": vlan,
+        "vlan_table": tablefy((vlan,)),
+        "attrs_table": tablefy(vlan.vlankeyvalue_set.all()),
+        "network_page_obj": network_paginator,
+        "network_table": tablefy(network_paginator),
     })
