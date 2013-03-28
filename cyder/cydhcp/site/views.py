e3748a3b4ba054a4336c2a00812b507f2f3ed7a5
fridgei <enkie1985@gmail.com>
2013-03-27 17:13:28 -0700

    site view is now using the new templates
    Need to get a clean way to paginate across multiple sets of page_objs

--- cyder/cydhcp/site/views.py
+++ cyder/cydhcp/site/views.py
@@ -1,60 +1,18 @@
-from django.core.exceptions import ValidationError
-from django.forms.util import ErrorList, ErrorDict
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 
 from cyder.base.utils import make_paginator, tablefy
-from cyder.cydhcp.keyvalue.utils import get_attrs, update_attrs
 from cyder.cydhcp.network.models import Network
-from cyder.cydhcp.site.models import Site, SiteKeyValue
-from cyder.cydhcp.site.forms import SiteForm
-
-
-def delete_site(request, site_pk):
-    get_object_or_404(Site, pk=site_pk)
-    return render(request, 'site/site_confirm_delete.html')
-
-
-def update_site(request, site_pk):
-    site = get_object_or_404(Site, pk=site_pk)
-    attrs = site.sitekeyvalue_set.all()
-    aux_attrs = SiteKeyValue.aux_attrs
-    if request.method == 'POST':
-        form = SiteForm(request.POST, instance=site)
-        try:
-            if form.is_valid():
-                # Handle KV store.
-                kv = get_attrs(request.POST)
-                update_attrs(kv, attrs, SiteKeyValue, site, 'site')
-                site = form.save()
-
-            return redirect(site)
-        except ValidationError, e:
-            if form._errors is None:
-                form._errors = ErrorDict()
-            form._errors['__all__'] = ErrorList(e.messages)
-            return render(request, 'site/site_edit.html', {
-                'site': site,
-                'form': form,
-                'attrs': attrs,
-                'aux_attrs': aux_attrs
-            })
-    else:
-        form = SiteForm(instance=site)
-        return render(request, 'site/site_edit.html', {
-            'site': site,
-            'form': form,
-            'attrs': attrs,
-            'aux_attrs': aux_attrs
-        })
+from cyder.cydhcp.site.models import Site
 
 
 def site_detail(request, site_pk):
     site = get_object_or_404(Site, pk=site_pk)
     networks = Network.objects.filter(site=site)
-
+    print networks
     return render(request, 'site/site_detail.html', {
-        'site': site,
-        'site_table': tablefy([site]),
+        'object': site,
+        'record_type': 'site',
+        'site_table': tablefy((site,)),
         'networks_table': tablefy(networks),
         'attrs_table': tablefy(site.sitekeyvalue_set.all()),
         'child_sites_table': tablefy(site.site_set.all()),
