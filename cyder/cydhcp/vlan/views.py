167a5ec824c434bed81bcb7e58b02f7a4451d22e
Kevin Ngo <ngoke@mozilla.com>
2013-03-29 14:23:08 -0700

    fix vlan detail view

--- cyder/cydhcp/vlan/views.py
+++ cyder/cydhcp/vlan/views.py
@@ -3,7 +3,7 @@ from cyder.cydhcp.vlan.models import Vlan
 
 
 def vlan_detail(request, pk):
-    return cy_detail(request, pk, Vlan, 'cydhcp/cydhcp_detail.html', {
+    return cy_detail(request, Vlan, 'cydhcp/cydhcp_detail.html', {
         'Networks': 'network_set',
         'Attributes': 'vlankeyvalue_set',
-    })
+    }, pk=pk)
