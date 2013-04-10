55978c8a6f15249479f2e4b67507b3d7fb6b1883
fridgei <enkie1985@gmail.com>
2013-04-09 19:43:27 -0700

    added subnet objects

--- parser.py
+++ parser.py
@@ -3,6 +3,8 @@ from ometa.grammar import OMeta
 from ometa.runtime import OMetaBase
 from string import letters, hexdigits
 from itertools import chain
+from ipaddr import IPv4Address, IPv6Address, IPv4Network, IPv6Network
+from collections import defaultdict
 import re
 import sys
 
@@ -33,6 +35,35 @@ class Option(object):
     def __eq__(self, other):
         return self.key == other.key and self.value == other.value
 
+
+class Pool(object):
+    def __init__(self, start, end, deny=None, allow=None, failover=None,
+                 options=None):
+        self.start = IPv4Address(start)
+        self.end = IPv4Address(end)
+        self.deny = deny or []
+        self.allow = allow or []
+        self.failover = failover or []
+
+
+def parse_to_pool(parse):
+    kwargs = {}
+    for key, value in chain(*(elem.items() for elem in parse)):
+        if key in kwargs:
+            kwargs[key].append(value)
+        else:
+            kwargs[key] = [value] if key not in ['start', 'end'] else value
+    return Pool(**kwargs)
+
+
+class Subnet(object):
+    def __init__(self, network_addr, netmask_addr, options=None,
+            statements=None, pools=None):
+        self.network_addr = IPv4Address(network_addr)
+        self.netmask_addr = IPv4Address(netmask_addr)
+        self.options = options or []
+        self.statements = statements or []
+        self.pools = pools or []
 
 mac_match = "(([0-9a-f]){2}:){5}([0-9a-f]){2}$"
 is_mac = re.compile(mac_match)
