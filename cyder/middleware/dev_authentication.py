aeaff04a3c69566673005c8e7b565af5d4f6df43
Nathan Murrow <nathan.murrow@oregonstate.edu>
2013-06-18 15:07:44 -0700

    dev auth middleware no longer forces test_superuser

--- cyder/middleware/dev_authentication.py
+++ cyder/middleware/dev_authentication.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 
 from cyder.core.cyuser.views import login_session
+from cyder.core.cyuser.models import User
 
 
 class DevAuthenticationMiddleware(object):
@@ -8,7 +9,11 @@ class DevAuthenticationMiddleware(object):
     def process_request(self, request):
         # Log in as development user.
         if 'ctnr' not in request.session:
-            request = login_session(request, 'test_superuser')
+            if '_auth_user_id' in request.session:
+                username = User.objects.get(pk=request.session['_auth_user_id'])
+            else:
+                username = 'test_superuser'
+            request = login_session(request, username)
 
         if request.path == '/logout/':
             request.session.flush()
