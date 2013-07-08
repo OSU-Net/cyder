b9062f78ed3e83be64c713d8902bad7619329090
church29 <joshchurch29@gmail.com>
2013-07-08 16:22:15 -0700

    build debug should now output

--- cyder/cydns/cybind/views.py
+++ cyder/cydns/cybind/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 
 from cyder.cydns.soa.models import SOA
 from cyder.cydns.cybind.zone_builder import build_zone_data
+from cyder.cydns.view.models import View
 
 import json as json
 
@@ -12,17 +13,23 @@ def build_debug_soa(request, soa_pk):
     soa = get_object_or_404(SOA, pk=soa_pk)
     #DEBUG_BUILD_STRING = build_zone(soa, root_domain)
     # Figure out what sort of domains are in this zone.
+    public_view = View.objects.get(id=1)
+    if View.objects.get(id=2):
+        private_view = View.objects.get(id=2)
     try:
-        private_data, public_data = build_zone_data(soa.root_domain, soa)
-        output = _("""
-;======= Private Data =======
-{0}
+        public_data = build_zone_data(public_view, soa.root_domain, soa)
+        if private_view:
+            private_data = build_zone_data(private_view, soa.root_domain, soa)
 
-;======= Private Data =======
-{1}
+        output = _("""
+                   ;======= Private Data =======
+                   {0}
+
+                   ;======= Private Data =======
+                   {1}
                    """.format(private_data, public_data))
     except Exception:
         return HttpResponse(json.dumps(
-                            {"error": "HOLY SHIT SOMETHING WENT WRONG!!!"}))
+                            {"error": "Could not build bind file"}))
     return render(request, 'cybind/sample_build.html',
                   {'data': output, 'soa': soa})
