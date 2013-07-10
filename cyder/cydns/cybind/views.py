ca9d15c3be1e15fa046b1ec5719b2657238904c6
Nathan Murrow <nathan.murrow@oregonstate.edu>
2013-07-10 12:08:07 -0700

    better error handling

--- cyder/cydns/cybind/views.py
+++ cyder/cydns/cybind/views.py
@@ -1,6 +1,7 @@
 from gettext import gettext as _
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.core.exceptions import ObjectDoesNotExist
 
 from cyder.cydns.soa.models import SOA
 from cyder.cydns.cybind.zone_builder import build_zone_data
@@ -13,18 +14,15 @@ def build_debug_soa(request, soa_pk):
     soa = get_object_or_404(SOA, pk=soa_pk)
     #DEBUG_BUILD_STRING = build_zone(soa, root_domain)
     # Figure out what sort of domains are in this zone.
-    public_view = View.objects.get(id=1)
-
-    if View.objects.filter(id=2).exists():
-        private_view = View.objects.get(id=2)
 
     try:
+        public_view = View.objects.get(name='public')
         public_data = build_zone_data(public_view, soa.root_domain, soa)
 
         try:
+            private_view = View.objects.get(name='private')
             private_data = build_zone_data(private_view, soa.root_domain, soa)
-
-        except:
+        except ObjectDoesNotExist:
             private_data = ''
 
         output = _("""
@@ -34,8 +32,10 @@ def build_debug_soa(request, soa_pk):
 ;======= Public Data =======
 {1}
                    """.format(private_data, public_data))
-    except Exception:
-        return HttpResponse(json.dumps(
-                            {"error": "Could not build bind file"}))
-    return render(request, 'cybind/sample_build.html',
-                  {'data': output, 'soa': soa})
+
+        return render(request, 'cybind/sample_build.html',
+                      {'data': output, 'soa': soa})
+
+    except Exception, e:
+        return HttpResponse(
+            json.dumps({"error": "Could not build bind file: %s" % e}))
