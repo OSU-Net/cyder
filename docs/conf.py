ce4a21bfa688e444ff6fadaa47e4e53b67b5a2fd
drkitty <bumblingkitty@gmail.com>
2013-07-13 17:56:30 -0700

    Get things imported correctly

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
-from django.conf import settings
-settings.configure()
+import manage
 
 # -- General configuration -----------------------------------------------------
 
