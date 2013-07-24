a538c6991162bb598746475d895b54aed1d66060
drkitty <bumblingkitty@gmail.com>
2013-07-24 13:45:34 -0700

    Add dhcp_build management command; fix settings

---
+++ cyder/cydhcp/management/commands/dhcp_build.py
@@ -0,0 +1,6 @@
+from cyder.cydhcp.build.builder import build
+from django.core.management.base import NoArgsCommand
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+        build()
