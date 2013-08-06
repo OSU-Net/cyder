aa7b6ff4aee3b30ca22c34c318dabc949a51e596
church29 <joshchurch29@gmail.com>
2013-08-05 17:33:36 -0700

    Static interfaces now link to their detail page through their fqdn

--- cyder/cydhcp/interface/static_intr/urls.py
+++ cyder/cydhcp/interface/static_intr/urls.py
@@ -8,4 +8,4 @@ urlpatterns = cydhcp_urls('static_interface') + patterns(
     url(r'^create/$', cydhcp_create,
         name='static_intr-create'),
     url(r'(?P<pk>[\w-]+)/$', static_intr_detail,
-        name='static_interface-details'),)
+        name='static_interface-detail'),)
