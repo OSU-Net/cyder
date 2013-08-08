fc0d6f5915eb6be446ffcdcdb319fb132294761d
Josh <joshchurch29@gmail.com>
2013-08-07 19:27:44 -0700

    Added ajax search for ranges

--- cyder/cydhcp/range/urls.py
+++ cyder/cydhcp/range/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import *
 
-from cyder.cydhcp.range.views import redirect_to_range_from_ip, range_detail
+from cyder.cydhcp.range.views import (redirect_to_range_from_ip, range_detail,
+                                      search)
 from cyder.cydhcp.urls import cydhcp_urls
 
 
 urlpatterns = cydhcp_urls('range') + patterns(
     '',
+    url(r'^search/', search, name='range-search'),
     url(r'^find_range/', redirect_to_range_from_ip, name='range-find'),
     url(r'^(?P<pk>[\w-]+)/$', range_detail, name='range-detail'),
 )
