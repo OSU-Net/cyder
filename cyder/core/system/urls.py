bb1a3985cdf102c3f5ea82e86af57f2b840d5619
Josh <joshchurch29@gmail.com>
2013-08-09 16:42:20 -0700

    System form autopopulates depending on where it is being called from

--- cyder/core/system/urls.py
+++ cyder/core/system/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, patterns
 
 from cyder.core.system.views import system_create_view, system_detail
 from cyder.core.urls import core_urls
@@ -7,5 +7,6 @@ from cyder.core.urls import core_urls
 urlpatterns = core_urls('system') + patterns(
     '',
     url(r'^(?P<pk>\d+)$', system_detail, name='system-detail'),
-    url(r'^create/', system_create_view, name='system-create'),
+    url(r'^create/(?P<initial>[\w.-]+)', system_create_view,
+        name='system-create'),
 )
