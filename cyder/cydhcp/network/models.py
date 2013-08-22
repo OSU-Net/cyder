142e2df3e6111c9b60fb7b85a38fcf18d308efb0
Josh <joshchurch29@gmail.com>
2013-08-21 17:04:37 -0700

    Fixed allocation errors not showing up when updating a network lower/prefixlen values

--- cyder/cydhcp/network/models.py
+++ cyder/cydhcp/network/models.py
@@ -123,11 +123,13 @@ class Network(models.Model, ObjectUrlMixin):
 
     def clean(self, *args, **kwargs):
         self.check_valid_range()
-        if (Network.objects.filter(prefixlen=self.prefixlen,
-                                   ip_upper=self.ip_upper,
-                                   ip_lower=self.ip_lower).exists()
-                and not self.id):
-            raise ValidationError("This network has already been allocated.")
+        allocated = Network.objects.filter(prefixlen=self.prefixlen,
+                                           ip_upper=self.ip_upper,
+                                           ip_lower=self.ip_lower)
+        if allocated:
+            if not self.id or self not in allocated:
+                raise ValidationError(
+                    "This network has already been allocated.")
 
         super(Network, self).clean(*args, **kwargs)
 
