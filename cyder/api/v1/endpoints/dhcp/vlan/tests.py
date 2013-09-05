d25f5fe6d22fbc3120e922ba624e90efa3eabc4a
Zane Salvatore <zeeman13@gmail.com>
2013-09-05 00:10:17 +0000

    Add vlan tests.

---
+++ cyder/api/v1/endpoints/dhcp/vlan/tests.py
@@ -0,0 +1,16 @@
+from cyder.api.v1.endpoints.dhcp.tests import DHCPAPITests
+from cyder.cydhcp.vlan.models import Vlan
+
+
+class VlanAPI_Test(DHCPAPITests):
+    model = Vlan
+    keyvalue_attr = "vlankeyvalue_set"
+
+    def create_data(self):
+        data = {
+            'name': 'vlan',
+            'number': 420,
+        }
+        obj, _ = self.model.objects.get_or_create(**data)
+        return obj
+
