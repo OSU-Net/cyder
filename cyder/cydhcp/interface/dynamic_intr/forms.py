cd80e02b84988ea7fc8e433c23c64cf0dd9f2a9c
Josh <joshchurch29@gmail.com>
2013-09-09 13:03:13 -0700

    Added fields to dynamic intr form

--- cyder/cydhcp/interface/dynamic_intr/forms.py
+++ cyder/cydhcp/interface/dynamic_intr/forms.py
@@ -2,9 +2,30 @@ from django import forms
 from cyder.cydhcp.interface.dynamic_intr.models import (DynamicInterface,
                                                         DynamicIntrKeyValue)
 from cyder.base.mixins import UsabilityFormMixin
+from cyder.cydhcp.vrf.models import Vrf
+from cyder.cydhcp.site.models import Site
+from cyder.cydhcp.range.models import Range
 
 
 class DynamicInterfaceForm(forms.ModelForm, UsabilityFormMixin):
+    vrf = forms.ModelChoiceField(
+        queryset=Vrf.objects.all(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'wizard'}))
+    site = forms.ModelChoiceField(
+        queryset=Site.objects.all(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'wizard'}))
+    range = forms.ModelChoiceField(
+        queryset=Range.objects.all(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'wizard'}))
+
+    def __init__(self, *args, **kwargs):
+        super(DynamicInterfaceForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = ['system', 'domain', 'mac', 'vrf',
+                                'site', 'range', 'workgroup', 'dhcp_enabled',
+                                'dns_enabled', 'ctnr']
 
     class Meta:
         model = DynamicInterface
