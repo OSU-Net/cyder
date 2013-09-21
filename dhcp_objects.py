e1084f098969e6dfac121fa7f8713f02bdc23ca7
drkitty <bumblingkitty@gmail.com>
2013-09-20 17:49:15 -0700

    Change everything

--- dhcp_objects.py
+++ dhcp_objects.py
@@ -1,21 +1,26 @@
 from itertools import ifilter
+from numbers import Number
 
 
 def join_p(xs, d=1):
     if not xs:
         return ''
     lines = "".join(map(str, xs)).splitlines()
-    return "".join('\t' * d + line + '\n' for line in lines)
+    prefix = '    ' * d if isinstance(d, Number) else d
+    return "".join(prefix + line + '\n' for line in lines)
 
 
-class EqualityMixin(object):
+class DHCPMixin(object):
+    side = ''
+
     def __ne__(self, other):
         return not self == other
 
+    def join_side(self, x):
+        return join_p(x, self.side)
 
-class Option(EqualityMixin):
-    friendly_name = 'option'
 
+class Option(DHCPMixin):
     def __init__(self, option, value):
         self.option = option
         self.value = value
@@ -24,18 +29,19 @@ class Option(EqualityMixin):
         if not isinstance(other, Option):
             return NotImplemented
         else:
-            return self.option == other.option and self.value == other.value
+            return (self.side, self.option, self.value ==
+                    other.side, other.option, other.value)
 
     def __hash__(self):
         return hash(self.option + self.value)
 
     def __str__(self):
-        return 'option {0} {1};\n'.format(self.option, self.value)
+        return self.join_side(
+            'option {0} {1};\n'.format(self.option, self.value)
+        )
 
 
-class Statement(EqualityMixin):
-    friendly_name = 'statement'
-
+class Statement(DHCPMixin):
     def __init__(self, statement):
         self.statement = statement
 
@@ -43,13 +49,14 @@ class Statement(EqualityMixin):
         if not isinstance(other, Statement):
             return NotImplemented
         else:
-            return self.statement == other.statement
+            return (self.side, self.statement ==
+                    other.side, other.statement)
 
     def __hash__(self):
         return hash(self.statement)
 
     def __str__(self):
-        return self.statement + ';\n'
+        return self.join_side(self.statement + ';\n')
 
 
 class RangeStmt(Statement):
@@ -59,9 +66,7 @@ class RangeStmt(Statement):
         self.end = end
 
 
-class Pool(EqualityMixin):
-    friendly_name = 'pool'
-
+class Pool(DHCPMixin):
     def __init__(self, contents):
         self.contents = set()
         for item in contents:
@@ -73,21 +78,22 @@ class Pool(EqualityMixin):
         if not isinstance(other, Pool):
             return NotImplemented
         else:
-            return (self.start, self.end) == (other.start, other.end)
+            return (self.side, self.start, self.end ==
+                    other.side, other.start, other.end)
 
     def __hash__(self):
         return hash(self.start + self.end)
 
     def __str__(self):
-        return ('pool {{\n'
-                '{0}'
-                '}}\n'
-                .format(join_p(self.contents)))
+        return self.join_side(
+            'pool {{\n'
+            '{0}'
+            '}}\n'
+            .format(join_p(self.contents))
+        )
 
 
-class Subnet(EqualityMixin):
-    friendly_name = 'subnet'
-
+class Subnet(DHCPMixin):
     def __init__(self, netaddr, netmask, contents):
         self.netaddr = netaddr
         self.netmask = netmask
@@ -97,50 +103,23 @@ class Subnet(EqualityMixin):
         if not isinstance(other, Subnet):
             return NotImplemented
         else:
-            return (self.netaddr, self.netmask) == (other.netaddr, other.netmask)
+            return (self.side, self.netaddr, self.netmask ==
+                    other.side, other.netaddr, other.netmask)
 
     def __hash__(self):
         return hash(self.netaddr + self.netmask)
 
     def __str__(self):
-        return ('subnet {0} netmask {1} {{\n'
-                '{2}'
-                '}}\n'
-                .format(self.netaddr, self.netmask, join_p(self.contents)))
-
-class Class(EqualityMixin):
-    friendly_name = 'class'
-
-    subclasses = set()
-
-    def __init__(self, name, contents):
-        self.name = name
-        self.contents = set(contents or [])
-
-    def __eq__(self, other):
-        if not isinstance(other, Class):
-            return NotImplemented
-        else:
-            return self.name == other.name
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def add_subclass(self, match, contents):
-        self.subclasses.update([Subclass(self.name, match, contents)])
-
-    def __str__(self):
-        return ('class "{0}" {{\n'
-                '{1}'
-                '}}\n'
-                '{2}'
-                .format(self.name, join_p(self.contents),
-                        join_p(self.subclasses, 0)))
+        return self.join_side(
+            'subnet {0} netmask {1} {{\n'
+            '{2}'
+            '}}\n'
+            .format(self.netaddr, self.netmask, join_p(self.contents))
+        )
 
 
-class Subclass(EqualityMixin):
-    friendly_name = 'subclass'
 
+class Subclass(DHCPMixin):
     def __init__(self, classname, match, contents):
         self.classname = classname
         self.match = match
@@ -150,8 +129,8 @@ class Subclass(EqualityMixin):
         if not isinstance(other, Subclass):
             return NotImplemented
         else:
-            return ((self.classname, self.match)
-                    == (other.classname, other.match))
+            return (self.side, self.classname, self.match ==
+                    other.side, other.classname, other.match)
 
     def __str__(self):
         if self.contents:
@@ -161,13 +140,51 @@ class Subclass(EqualityMixin):
                    .format(join_p(self.contents)))
         else:
             end = ';\n'
-        return ('subclass "{0}" {1}{2}'
-                .format(self.classname, self.match, end))
+        return self.join_side(
+            'subclass "{0}" {1}{2}'
+            .format(self.classname, self.match, end)
+        )
 
 
-class Group(EqualityMixin):
-    friendly_name = 'group'
+class Class(DHCPMixin):
+    def __init__(self, name, contents):
+        self.name = name
+        self.contents = set(contents or [])
 
+    def __eq__(self, other):
+        if not isinstance(other, Class):
+            return NotImplemented
+        else:
+            return (self.side, self.name ==
+                    other.side, other.name)
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def add_subclass(self, match, contents):
+        self.contents.update([Subclass(self.name, match, contents)])
+
+    @property
+    def subclasses(self):
+        return set(ifilter(lambda x: isinstance(x, Subclass), self.contents))
+
+    @property
+    def truecontents(self):
+        return set(ifilter(lambda x: not isinstance(x, Subclass),
+                   self.contents))
+
+    def __str__(self):
+        return self.join_side(
+            'class "{0}" {{\n'
+            '{1}'
+            '}}\n'
+            '{2}'
+            .format(self.name, join_p(self.truecontents),
+                    join_p(self.subclasses, 0))
+        )
+
+
+class Group(DHCPMixin):
     def __init__(self, name, contents):
         self.name = name
         self.contents = set(contents or [])
@@ -176,45 +193,55 @@ class Group(EqualityMixin):
         if not isinstance(other, Group):
             return NotImplemented
         else:
-            return self.name == other.name
+            return (self.side, self.name ==
+                    other.side, other.name)
 
     def __hash__(self):
         return hash(self.name)
 
     def __str__(self):
-        return ('group {{ # {0}\n'
-                '{1}'
-                '}}\n'
-                .format(self.name, join_p(self.contents)))
+        return self.join_side(
+            'group {{ # {0}\n'
+            '{1}'
+            '}}\n'
+            .format(self.name, join_p(self.contents))
+        )
 
 
-class Host(EqualityMixin):
-    friendly_name = 'host'
-
+class Host(DHCPMixin):
     def __init__(self, name, contents):
         self.name = name
-        self.contents = contents
+        self.contents = set(contents or [])
 
     def __eq__(self, other):
         if not isinstance(other, Host):
             return NotImplemented
         else:
-            return self.name == other.name
+            return (self.side, self.name ==
+                    other.side, other.name)
 
     def __hash__(self):
         return hash(self.name)
 
     def __str__(self):
-        return ('host {0} {{\n'
-                '{1}'
-                '}}\n'
-                .format(self.name, join_p(self.contents)))
+        return self.join_side(
+            'host {0} {{\n'
+            '{1}'
+            '}}\n'
+            .format(self.name, join_p(self.contents))
+        )
 
 
-class ConfigFile(EqualityMixin):
-    friendly_name = 'config file'
+class ConfigFile(DHCPMixin):
+    def __init__(self):
+        self.contents = set()
 
-    contents = set()
+    def __eq__(self, other):
+        return NotImplemented
+
+
+    def __hash__(self):
+        return NotImplemented
 
     def add(self, item):
         if item:
--- parser.py
+++ parser.py
@@ -1,45 +1,119 @@
 import re
+from copy import deepcopy
+from itertools import ifilter
 from parsley import makeGrammar
-
+from sys import stdout
 
 from dhcp_objects import (Option, Statement, RangeStmt, Pool, Subnet, Class,
                           Subclass, Group, Host, ConfigFile)
 
+
+
+def first(it):
+    return next(it, None)
+
 symbols = reduce(lambda x,y: x + y,
                  (chr(i) for i in xrange(0x21, 0x7E + 1)))
 
-
-config = ConfigFile()
-
-
-bindings = {
-    'symbols': symbols,
-    'config': config,
-    'Option': Option,
-    'Statement': Statement,
-    'RangeStmt': RangeStmt,
-    'Pool': Pool,
-    'Subnet': Subnet,
-    'Class': Class,
-    'Subclass': Subclass,
-    'Group': Group,
-    'Host': Host,
-}
-
-with open('dhcp.parsley') as f:
-    grammar = makeGrammar(f.read(), bindings)
-
 comment = re.compile(r'(?:"(?:[^"\\]|\\.)*"|[^"#])*(#|$)')
 
-with open('dhcpd.conf') as f:
-    fStr = ''
-    for line in f:
-        if not line.startswith('group'):
-            line = line[:comment.match(line).start(1)]
-            if line[-1] != '\n':
-                line += '\n' # not strictly necessary
-        fStr += line
 
-    x = grammar(fStr)
+def parsefile(name):
+    config = ConfigFile()
 
-x.configFile()
+    bindings = {
+        'symbols': symbols,
+        'config': config,
+        'Option': Option,
+        'Statement': Statement,
+        'RangeStmt': RangeStmt,
+        'Pool': Pool,
+        'Subnet': Subnet,
+        'Class': Class,
+        'Subclass': Subclass,
+        'Group': Group,
+        'Host': Host,
+    }
+
+    with open('dhcp.parsley') as f:
+        grammar = makeGrammar(f.read(), bindings)
+
+    with open(name) as f:
+        fStr = ''
+        for line in f:
+            if not line.startswith('group'):
+                line = line[:comment.match(line).start(1)]
+                if line[-1] != '\n':
+                    line += '\n' # not strictly necessary
+            fStr += line
+
+    g = grammar(fStr)
+    g.configFile()
+
+    #import pdb; pdb.set_trace()
+
+    return config
+
+
+def find_in(obj, xs):
+    return first(ifilter(lambda x: x == obj, xs))
+
+
+def add_all(x, diff, side):
+    cx = deepcopy(x)
+    cx.side = side
+    diff.contents.update([cx]) # add immediately
+    if hasattr(x, 'contents'):
+        for n in x.contents:
+            add_all(n, cx, side)
+
+
+def compare(left, right, diff):
+    if isinstance(left, Host) and left.name == 'google.com':
+        import pdb; pdb.set_trace()
+
+    if hasattr(left, 'contents'):
+        same = True
+
+        # set intersection is broken, but there's no way to fix it
+        for x in left.contents:
+            if x in right.contents: # both left and right
+                cx = deepcopy(x)
+                cx.contents = set()
+                cx.side = ' '
+                y = find_in(x, right.contents)
+                if not compare(x, y, cx): # if they're not the same
+                    diff.contents.update([cx])
+                    same = False
+            else: # only left
+                add_all(x, diff, '<')
+                same = False
+
+        for y in right.contents - left.contents: # only right
+            add_all(y, diff, '>')
+            same = False
+    else:
+        same = (left == right) # simple comparison
+
+    stdout.write('================================\n')
+    stdout.write(str(left))
+    stdout.write('=========== l & r ==============\n')
+    stdout.write(str(right))
+    stdout.write('=========== diff ===============\n')
+    stdout.write(str(diff))
+    stdout.write('================================\n')
+
+    return same
+
+
+diffFile = None
+def do_it():
+    global diffFile
+    one = parsefile('dhcpd1.conf')
+    two = parsefile('dhcpd2.conf')
+    diffFile = ConfigFile()
+
+    compare(one, two, diffFile)
+
+if __name__ == '__main__':
+    do_it()
