9a4e35b86265e438fc069339ada2796d00d154a6
Josh <joshchurch29@gmail.com>
2013-09-30 17:22:16 -0700

    Fixed extra _ in kwargs

--- cyder/cydhcp/range/models.py
+++ cyder/cydhcp/range/models.py
@@ -141,7 +141,7 @@ class Range(models.Model, ObjectUrlMixin):
 
     def save(self, *args, **kwargs):
         self.clean()
-        update_range_usage = kwargs.pop('update__range_usage', True)
+        update_range_usage = kwargs.pop('update_range_usage', True)
         if update_range_usage:
             self.range_usage = self.get_usage()
         super(Range, self).save(*args, **kwargs)
