b99a51525a70231477feadf4baa6ff141f15ba93
Zane Salvatore <zeeman13@gmail.com>
2013-10-02 16:40:31 -0700

    All tests passing.

--- cyder/api/v1/endpoints/dns/ptr/tests.py
+++ cyder/api/v1/endpoints/dns/ptr/tests.py
@@ -12,7 +12,8 @@ class PTRBase(DNSAPITests):
         return {
             'description': 'PTR Record',
             'ttl': 420,
-            'name': 'ptr',
+            'label': 'ptr',
+            'domain': self.domain,
         }
 
 
@@ -20,14 +21,14 @@ class PTRv4API_Test(PTRBase):
     model = PTR
 
     def create_data(self):
-        super(PTRv4API_Test, self).create_data()
+        data = super(PTRv4API_Test, self).create_data()
         Domain.objects.get_or_create(name='in-addr.arpa')
         Domain.objects.get_or_create(name='11.in-addr.arpa')
 
-        data = {
+        data.update({
             'ip_str': '11.1.2.3',
             'ip_type': '4',
-        }
+        })
 
         obj, _ = self.model.objects.get_or_create(**data)
         return obj
@@ -37,14 +38,14 @@ class PTRv6API_Test(PTRBase):
     model = PTR
 
     def create_data(self):
-        super(PTRv6API_Test, self).create_data()
+        data = super(PTRv6API_Test, self).create_data()
         Domain.objects.get_or_create(name='ip6.arpa')
         Domain.objects.get_or_create(name='1.ip6.arpa')
 
-        data = {
+        data.update({
             'ip_str': '1000:2000:3000:0000:0000:4000:5000:6000',
             'ip_type': '6',
-        }
+        })
 
         obj, _ = self.model.objects.get_or_create(**data)
         return obj
