6918ae2a5b0fc1b1016fdf046b3f935c91a828cd
drkitty <bumblingkitty@gmail.com>
2013-10-14 19:02:54 -0700

    Remove import-only "tests"

--- cyder/cydhcp/tests/all.py
+++
@@ -1,5 +0,0 @@
-from cyder.cydhcp.interface.static_intr.tests.all import *
-from cyder.cydhcp.range.tests.all import *
-from cyder.cydhcp.network.tests import *
-from cyder.cydhcp.lib.tests.all import *
-from cyder.cydhcp.tests.test_views import *
