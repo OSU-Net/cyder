90b24465900f801cb5b0d4474b7fbe6820f99cc9
Nathan Murrow <nathan.murrow@oregonstate.edu>
2013-10-15 14:17:31 -0700

    remove domain purge from ipv4_tests

--- cyder/cydhcp/range/tests/ipv4_tests.py
+++ cyder/cydhcp/range/tests/ipv4_tests.py
@@ -12,7 +12,6 @@ from cyder.core.ctnr.models import Ctnr
 class V4RangeTests(TestCase):
 
     def setUp(self):
-        Domain.objects.all().delete()
         self.ctnr = Ctnr(name='abloobloobloo')
         self.ctnr.save()
         self.d = Domain(name="com")
