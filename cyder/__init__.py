76c03c47b20554cc5a9420a91b4f6020cf6237bf
drkitty <bumblingkitty@gmail.com>
2013-11-01 20:43:15 -0700

    Fix some more stuff

--- cyder/__init__.py
+++ cyder/__init__.py
@@ -6,8 +6,8 @@ from django.db.models.signals import post_syncdb
 from south.signals import post_migrate
 
 
-# South doesn't automatically load custom SQL like Django does.
-
+# South doesn't automatically load custom SQL like Django does, and regardless,
+# the filename isn't what Django would expect.
 def _load_custom_sql():
     from django.db import connection
 
@@ -16,18 +16,24 @@ def _load_custom_sql():
         cursor.execute(f.read())
 
 
-@receiver(post_migrate)
-def _post_migrate(**kwargs):
-    print '*** ' + kwargs['app']
+def _load_fixtures():
+    from django.core.management import call_command
+    from os import listdir
 
-    _load_custom_sql()
+    for filename in listdir('cyder/initial_data'):
+        call_command('loaddata', 'cyder/initial_data/' + filename)
 
 
 @receiver(post_syncdb)
 def _post_syncdb(**kwargs):
-    print '*** ' + kwargs['sender'].__name__
-
     from cyder.settings import TESTING
 
-    if TESTING: # and kwargs['sender'] == 'cyder':
+    if TESTING and kwargs['sender'].__name__ == 'cyder.models':
         _load_custom_sql()
+        _load_fixtures()
+
+
+@receiver(post_migrate)
+def _post_migrate(**kwargs):
+    _load_custom_sql()
+    _load_fixtures()
