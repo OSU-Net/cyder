d2b3ac8db6f748365168bfcd951f55d0bf3ff86b
drkitty <bumblingkitty@gmail.com>
2013-11-01 18:04:21 -0700

    Move all custom SQL into a single file

---
+++ cyder/sql/cyder.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `address_record` CHANGE `ip_upper` `ip_upper` BIGINT( 64 ) UNSIGNED;
+ALTER TABLE `address_record` CHANGE `ip_lower` `ip_lower` BIGINT( 64 ) UNSIGNED;
+
+ALTER TABLE `ip` CHANGE `ip_upper` `ip_upper` BIGINT( 64 ) UNSIGNED;
+ALTER TABLE `ip` CHANGE `ip_lower` `ip_lower` BIGINT( 64 ) UNSIGNED;
+
+ALTER TABLE `network` CHANGE `ip_upper` `ip_upper` BIGINT( 64 ) UNSIGNED;
+ALTER TABLE `network` CHANGE `ip_lower` `ip_lower` BIGINT( 64 ) UNSIGNED;
+
+ALTER TABLE `ptr` CHANGE `ip_upper` `ip_upper` BIGINT( 64 ) UNSIGNED NOT NULL;
+ALTER TABLE `ptr` CHANGE `ip_lower` `ip_lower` BIGINT( 64 ) UNSIGNED NOT NULL;
+
+ALTER TABLE `range` CHANGE `start_upper` `start_upper` BIGINT( 64 ) UNSIGNED;
+ALTER TABLE `range` CHANGE `start_lower` `start_lower` BIGINT( 64 ) UNSIGNED;
+ALTER TABLE `range` CHANGE `end_upper` `end_upper` BIGINT( 64 ) UNSIGNED;
+ALTER TABLE `range` CHANGE `end_lower` `end_lower` BIGINT( 64 ) UNSIGNED;
