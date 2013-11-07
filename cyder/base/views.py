f6bec9232d43ce4a0e7620b0b360d8370066ea83
Josh <joshchurch29@gmail.com>
2013-11-07 15:57:54 -0800

    EAV forms now submit with an ajax query fixing weird redirects on errors

--- cyder/base/views.py
+++ cyder/base/views.py
@@ -156,6 +156,10 @@ def cy_view(request, get_klasses_fn, template, pk=None, obj_type=None):
                             not obj.ctnr_set.all().exists()):
                         obj.ctnr_set.add(request.session['ctnr'])
 
+                    # to modify forms to submit with ajax, adjust this if
+                    if 'AV' in FormKlass.__name__:
+                        return HttpResponse(json.dumps({'success': True}))
+
                     return redirect(
                         request.META.get('HTTP_REFERER', obj.get_list_url()))
 
@@ -177,6 +181,10 @@ def cy_view(request, get_klasses_fn, template, pk=None, obj_type=None):
 
     if issubclass(type(form), UsabilityFormMixin):
         form.make_usable(request)
+
+    # to modify forms to submit with ajax, adjust this if
+    if 'AV' in FormKlass.__name__:
+        return HttpResponse(json.dumps({'errors': form.errors}))
 
     if obj_type == 'system' and len(object_list) == 0:
         return redirect(reverse('system-create', args=[None]))
