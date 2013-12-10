12d16577e53c48c227909efdd09c00ab57faefbe
church29 <joshchurch29@gmail.com>
2013-12-10 13:40:35 -0800

    Fixed broken user detail view

--- cyder/base/views.py
+++ cyder/base/views.py
@@ -306,15 +306,24 @@ def cy_detail(request, Klass, template, obj_sets, pk=None, obj=None, **kwargs):
             obj_set = getattr(obj, obj_set).all()
         page_obj = make_paginator(
             request, obj_set, obj_type=name.lower().replace(' ', ''))
+        if obj_type == 'user':
+            table = tablefy(page_obj, request=request, update=False)
+        else:
+            table = tablefy(page_obj, request=request)
+
         tables.append({
             'name': name,
             'page_obj': page_obj,
-            'table': tablefy(page_obj, request=request)
+            'table': table
         })
+    if obj_type == 'user':
+        table = tablefy((obj,), info=False, request=request, update=False)
+    else:
+        table = tablefy((obj,), info=False, request=request)
 
     return render(request, template, dict({
         'obj': obj,
-        'obj_table': tablefy((obj,), info=False, request=request),
+        'obj_table': table,
         'obj_type': obj_type,
         'tables': tables
     }.items() + kwargs.items()))
