403a11b541722d2f067306871d65a7aac0f5d1e2
Nathan Murrow <nathan.murrow@oregonstate.edu>
2014-01-11 17:52:11 -0800

    add expire_interfaces command

---
+++ cyder/management/commands/expire_interfaces.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from cyder.models import MODEL_LIST
+from cyder.base.models import ExpirableMixin
+
+from datetime import datetime
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list
+
+    def handle(self, *args, **options):
+        now = datetime.now()
+        for model in MODEL_LIST:
+            if issubclass(model, ExpirableMixin):
+                model.objects.filter(expire__lte=now).delete()
