16dc6afc0d0b2b7c54aff573867dee3ede9a7a33
Zane Salvatore <zeeman13@gmail.com>
2014-01-22 17:43:28 -0800

    Get rid of redundant file

--- docs/api/examples/csv.py
+++ docs/api/examples/csv.py
@@ -5,68 +5,72 @@ import sys
 from conrad import Conrad
 
 
-API_TOKEN = "3488d442b17d7e3c8d55f3ed60c660cface5d9ab"
+API_TOKEN = "4df242bda29ab46e9343ed1042a557fa9540935c"
 BASE_URL = "http://127.0.0.1:8000/api/v1/"
 
-HOSTS = {"path": "core/system", "attributes": []}
 
-
-def api_get_csv(path, fields=None, params=None, attributes=None, separator=";",
+def api_get_csv(path, fields=None, params=None, attributes=None, delimiter=";",
                 limit=None):
     """
     Params:
-      - path    The specific path to access.
-      - fields  The fields to export as CSV. This field is optional, but not
-                passing it may lead to strange results.
-      - params  Any query string parameters to pass with the request, stored as
-                a dict.
+      - path        The specific path to access.
+      - fields      The fields to export as CSV. This field is optional, but not
+                    passing it may lead to strange results.
+      - params      Any query string parameters to pass with the request, stored
+                    as a dict.
       - attributes  Attributes to retrieve the values of.
-      - separator   The character to separate values with. Set to ';' by
+      - delimiter   The character to separate values with. Set to ';' by
                     default.
 
     Return:
         The results of the API request serialized in a CSV format.
     """
-
     c = Conrad(API_TOKEN, BASE_URL)
 
-    c.get(path, params)
+    c.get(path.strip('/'), params)
 
     if fields is None:
         # Assume that the top-level fields are the same for every record.
         fields = c.result[0].keys()
 
     # Build the header row.
-    csv = separator.join(fields)
+    header = delimiter.join(fields)
     if attributes:
-        csv += (separator + separator.join(attributes))
-    csv += "\n"
+        header += (delimiter + delimiter.join(attributes))
+    header += "\n"
+    sys.stdout.write(header)
 
     num_results = 0
+
+    line = ""
 
     while True:
         for record in c.result:
             for field in fields:
-                csv += (str(record[field]) + separator)
+                line += (str(record[field]) + delimiter)
 
             if attributes:
-                keyvalues = record[
-                    next(itertools.ifilter(lambda x: x.endswith("av_set"),
-                                           record),
-                         None)]
+                avset_name = next(itertools.ifilter(
+                    lambda x: x.endswith("av_set"), record), None)
+                if avset_name is None:
+                    raise Exception("You tried to filter by attributes, but "
+                                    "this record type does not appear to "
+                                    "support them.")
+
+                keyvalues = record[avset_name]
 
                 for attribute in attributes:
                     for kv in keyvalues:
-                        if kv['attribute'] == attribute:
-                            csv += (str(kv['value']) + separator)
+                        if kv['attribute'].lower() == attribute.lower():
+                            line += (str(kv['value']) + delimiter)
                             break
                     else:
                         # Not found, but we still want a blank cell where the
                         # value would be.
-                        csv += separator
+                        line += delimiter
 
-            sys.stdout.write(csv + '\n')
-            csv = ""
+            print line
+            line = ""
             num_results += 1
             if limit and num_results >= limit:
                 break
@@ -92,7 +96,7 @@ def main():
     parser.add_argument('--params', '-p', type=str,
                         help="Query string parameters to submit with the "
                              "request, in query string format.")
-    parser.add_argument('--separator', '-s', type=str, default=';',
+    parser.add_argument('--delimiter', '-d', type=str, default=';',
                         help="The character to separate values with.")
     parser.add_argument('--limit', '-l', type=int,
                         help="The number of records to retrieve before "
@@ -103,9 +107,9 @@ def main():
     if args.attributes:
         args.attributes = args.attributes.split(',')
 
-    #sys.stdout.write(api_get_csv(args.path, fields=args.fields,
-    #                             params=args.params, attributes=args.attributes,
-    #                             separator=args.separator, limit=args.limit))
+    if args.fields:
+        args.fields = args.fields.split(',')
+
     api_get_csv(**vars(args))
 
 if __name__ == "__main__":
