0de02d0b9dc3bb923c5457942b9de1854f74ad2c
drkitty <bumblingkitty@gmail.com>
2014-02-17 18:50:10 -0800

    Add some more DHCP tests

--- cyder/cydhcp/build/tests/build_tests.py
+++ cyder/cydhcp/build/tests/build_tests.py
@@ -3,10 +3,14 @@ from django.test import TestCase
 
 from cyder.base.eav.models import Attribute
 from cyder.base.utils import copy_tree, remove_dir_contents
-from cyder.base.vcs import GitRepo
-from cyder.cydhcp.build.builder import DHCPBuilder
-from cyder.cydhcp.network.models import Network, NetworkAV
+from cyder.base.vcs import GitRepo, SanityCheckFailure
 
+from cyder.core.ctnr.models import Ctnr
+from cyder.core.system.models import System
+
+from cyder.cydhcp.build.builder import DHCPBuilder
+from cyder.cydhcp.interface.dynamic_intr.models import DynamicInterface
+from cyder.cydhcp.network.models import Network, NetworkAV
 from cyder.cydhcp.range.models import Range
 
 
@@ -57,13 +61,14 @@ class DHCPBuildTest(TestCase):
         super(DHCPBuildTest, self).setUp()
 
     def test_build_and_push(self):
+        """Test that adding data triggers a rebuild"""
         self.builder.build()
-        self.builder.push()
+        self.builder.push(sanity_check=False)
 
         rev1 = self.builder.repo.get_revision()
 
         self.builder.build()
-        self.builder.push()
+        self.builder.push(sanity_check=False)
 
         rev2 = self.builder.repo.get_revision()
 
@@ -77,8 +82,71 @@ class DHCPBuildTest(TestCase):
         ).save()
 
         self.builder.build()
-        self.builder.push()
+        self.builder.push(sanity_check=False)
 
         rev3 = self.builder.repo.get_revision()
 
         self.assertNotEqual(rev2, rev3)
+
+    def test_sanity_check1(self):
+        """Test that the sanity check fails when too many lines are changed"""
+
+        self.builder.repo.line_change_limit = 1
+        self.builder.repo.line_removal_limit = 100
+
+        self.builder.build()
+        self.builder.push(sanity_check=False)
+
+        d = DynamicInterface(
+            system=System.objects.get(name='Test_system_5'),
+            mac='ab:cd:ef:ab:cd:ef',
+            range=Range.objects.get(name='Test range 1'),
+            ctnr=Ctnr.objects.get(name='Global'),
+        )
+        d.full_clean()
+        d.save()
+
+        self.builder.build()
+        def bad_push():
+            self.builder.push(sanity_check=True)
+        self.assertRaises(SanityCheckFailure, bad_push)
+
+    def test_sanity_check2(self):
+        """Test that the sanity check fails when too many lines are removed"""
+
+        self.builder.repo.line_change_limit = 100
+        self.builder.repo.line_removal_limit = 1
+
+        self.builder.build()
+        self.builder.push(sanity_check=False)
+
+        DynamicInterface.objects.filter(
+            mac__in=('010204081020', 'aabbccddeeff')).delete()
+
+        self.builder.build()
+        def bad_push():
+            self.builder.push(sanity_check=True)
+        self.assertRaises(SanityCheckFailure, bad_push)
+
+    def test_sanity_check3(self):
+        """Test that the sanity check succeeds when changes are sane"""
+
+        self.builder.repo.line_change_limit = 100
+        self.builder.repo.line_removal_limit = 100
+
+        self.builder.build()
+        self.builder.push(sanity_check=False)
+
+        DynamicInterface.objects.filter(
+            mac__in=('010204081020', 'aabbccddeeff')).delete()
+        d = DynamicInterface(
+            system=System.objects.get(name='Test_system_5'),
+            mac='ab:cd:ef:ab:cd:ef',
+            range=Range.objects.get(name='Test range 1'),
+            ctnr=Ctnr.objects.get(name='Global'),
+        )
+        d.full_clean()
+        d.save()
+
+        self.builder.build()
+        self.builder.push(sanity_check=True)
