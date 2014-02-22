4fc2a9aa3f844e7046508318cb8bc76052838a47
drkitty <bumblingkitty@gmail.com>
2014-02-22 14:39:50 -0800

    Stop using funfactory manage and settings_base

--- cyder/settings/base.py
+++ cyder/settings/base.py
@@ -1,10 +1,234 @@
 # This is Cyder's main settings file. If you need to override a setting
 # locally, use cyder/settings/local.py
 
+import glob
+import itertools
+import logging
 import os
+import socket
 import sys
+from django.utils.functional import lazy
 
-from funfactory.settings_base import *
+from lib.path_utils import ROOT, path
+
+##########################
+# copied from funfactory #
+##########################
+
+SLAVE_DATABASES = []
+
+DATABASE_ROUTERS = ('multidb.PinningMasterSlaveRouter',)
+
+## Logging
+LOG_LEVEL = logging.INFO
+HAS_SYSLOG = True
+SYSLOG_TAG = "http_app_playdoh"  # Change this after you fork.
+LOGGING_CONFIG = None
+LOGGING = {}
+
+# CEF Logging
+CEF_PRODUCT = 'Playdoh'
+CEF_VENDOR = 'Mozilla'
+CEF_VERSION = '0'
+CEF_DEVICE_VERSION = '0'
+
+## Accepted locales
+
+# Tells the product_details module where to find our local JSON files.
+# This ultimately controls how LANGUAGES are constructed.
+PROD_DETAILS_DIR = path('lib/product_details_json')
+
+# On dev instances, the list of accepted locales defaults to the contents of
+# the `locale` directory within a project module or, for older Playdoh apps,
+# the root locale directory.  A localizer can add their locale in the l10n
+# repository (copy of which is checked out into `locale`) in order to start
+# testing the localization on the dev server.
+try:
+    DEV_LANGUAGES = [
+        os.path.basename(loc).replace('_', '-')
+        for loc in itertools.chain(glob.iglob(ROOT + '/locale/*'),  # old style
+                                   glob.iglob(ROOT + '/*/locale/*'))
+        if (os.path.isdir(loc) and os.path.basename(loc) != 'templates')
+    ]
+except OSError:
+    DEV_LANGUAGES = ('en-US',)
+
+
+def lazy_lang_url_map():
+    from django.conf import settings
+    langs = settings.DEV_LANGUAGES if settings.DEV else settings.PROD_LANGUAGES
+    return dict([(i.lower(), i) for i in langs])
+
+LANGUAGE_URL_MAP = lazy(lazy_lang_url_map, dict)()
+
+
+# Override Django's built-in with our native names
+def lazy_langs():
+    from django.conf import settings
+    from product_details import product_details
+    langs = DEV_LANGUAGES if settings.DEV else settings.PROD_LANGUAGES
+    return dict([(lang.lower(), product_details.languages[lang]['native'])
+                 for lang in langs if lang in product_details.languages])
+
+LANGUAGES = lazy(lazy_langs, dict)()
+
+# Tells the extract script what files to look for L10n in and what function
+# handles the extraction. The Tower library expects this.
+DOMAIN_METHODS = {
+    'messages': [
+        # Searching apps dirs only exists for historic playdoh apps.
+        # See playdoh's base settings for how message paths are set.
+        ('apps/**.py',
+            'tower.management.commands.extract.extract_tower_python'),
+        ('apps/**/templates/**.html',
+            'tower.management.commands.extract.extract_tower_template'),
+        ('templates/**.html',
+            'tower.management.commands.extract.extract_tower_template'),
+    ],
+}
+
+# Paths that don't require a locale code in the URL.
+SUPPORTED_NONLOCALES = ['media', 'static', 'admin']
+
+## Media and templates.
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = path('static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'jingo.Loader',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'session_csrf.context_processor',
+    'django.contrib.messages.context_processors.messages',
+    'funfactory.context_processors.i18n',
+    'funfactory.context_processors.globals',
+    #'jingo_minify.helpers.build_ids',
+)
+
+
+def get_template_context_processors(exclude=(), append=(),
+                        current={'processors': TEMPLATE_CONTEXT_PROCESSORS}):
+    """
+    Returns TEMPLATE_CONTEXT_PROCESSORS without the processors listed in
+    exclude and with the processors listed in append.
+
+    The use of a mutable dict is intentional, in order to preserve the state of
+    the TEMPLATE_CONTEXT_PROCESSORS tuple across multiple settings files.
+    """
+
+    current['processors'] = tuple(
+        [p for p in current['processors'] if p not in exclude]
+    ) + tuple(append)
+
+    return current['processors']
+
+
+TEMPLATE_DIRS = (
+    path('templates'),
+)
+
+# Storage of static files
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter'
+)
+COMPRESS_PRECOMPILERS = (
+    #('text/coffeescript', 'coffee --compile --stdio'),
+    ('text/less', 'lessc {infile} {outfile}'),
+    #('text/x-sass', 'sass {infile} {outfile}'),
+    #('text/x-scss', 'sass --scss {infile} {outfile}'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+
+def JINJA_CONFIG():
+    import jinja2
+    from django.conf import settings
+#    from caching.base import cache
+    config = {'extensions': ['tower.template.i18n', 'jinja2.ext.do',
+                             'jinja2.ext.with_', 'jinja2.ext.loopcontrols'],
+              'finalize': lambda x: x if x is not None else ''}
+#    if 'memcached' in cache.scheme and not settings.DEBUG:
+        # We're passing the _cache object directly to jinja because
+        # Django can't store binary directly; it enforces unicode on it.
+        # Details: http://jinja.pocoo.org/2/documentation/api#bytecode-cache
+        # and in the errors you get when you try it the other way.
+#        bc = jinja2.MemcachedBytecodeCache(cache._cache,
+#                                           "%sj2:" % settings.CACHE_PREFIX)
+#        config['cache_size'] = -1 # Never clear the cache
+#        config['bytecode_cache'] = bc
+    return config
+
+# Path to Java. Used for compress_assets.
+JAVA_BIN = '/usr/bin/java'
+
+# Sessions
+#
+# By default, be at least somewhat secure with our session cookies.
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+
+## Tests
+TEST_RUNNER = 'test_utils.runner.RadicalTestSuiteRunner'
+
+## Celery
+
+# True says to simulate background tasks without actually using celeryd.
+# Good for local development in case celeryd is not running.
+CELERY_ALWAYS_EAGER = True
+
+BROKER_CONNECTION_TIMEOUT = 0.1
+CELERY_RESULT_BACKEND = 'amqp'
+CELERY_IGNORE_RESULT = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+# Time in seconds before celery.exceptions.SoftTimeLimitExceeded is raised.
+# The task can catch that and recover but should exit ASAP.
+CELERYD_TASK_SOFT_TIME_LIMIT = 60 * 2
+
+## Arecibo
+# when ARECIBO_SERVER_URL is set, it can use celery or the regular wrapper
+ARECIBO_USES_CELERY = True
+
+# For absolute urls
+try:
+    DOMAIN = socket.gethostname()
+except socket.error:
+    DOMAIN = 'localhost'
+PROTOCOL = "http://"
+PORT = 80
+
+## django-mobility
+MOBILE_COOKIE = 'mobile'
+
+
+#########
+# Cyder #
+#########
+
+
 from cyder.settings.dns import *
 
 TESTING = True if sys.argv[1:] and sys.argv[1] == 'test' else False
@@ -100,7 +324,38 @@ MINIFY_BUNDLES = {
     }
 }
 
-INSTALLED_APPS = list(INSTALLED_APPS) + [
+INSTALLED_APPS = [
+    # Local apps
+    'funfactory',  # Content common to most playdoh-based apps.
+    'compressor',
+
+    'tower',  # for ./manage.py extract (L10n)
+    'cronjobs',  # for ./manage.py cron * cmd line tasks
+    'django_browserid',
+
+
+    # Django contrib apps
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    # 'django.contrib.sites',
+    # 'django.contrib.messages',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+
+    # Third-party apps, patches, fixes
+    'commonware.response.cookies',
+    'djcelery',
+    'django_nose',
+    'session_csrf',
+
+    # L10n
+    'product_details',
+
+    # Cyder
     'cyder',
 
     # Third party apps
@@ -183,6 +438,10 @@ BASE_PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
 )
+HMAC_KEYS = {  # for bcrypt only
+    #'2012-06-06': 'cheesecake',
+}
+
 from django_sha2 import get_password_hashers
 PASSWORD_HASHERS = get_password_hashers(BASE_PASSWORD_HASHERS, HMAC_KEYS)
 
@@ -302,3 +561,38 @@ DATETIME_INPUT_FORMATS = (
     '%m/%d/%y %H:%M:%S',     # '10/25/06 14:30:59'
     '%m/%d/%y',              # '10/25/06'
 )
+
+###############################
+# more copied from funfactory #
+###############################
+
+## Middlewares, apps, URL configs.
+
+def get_middleware(exclude=(), append=(),
+                   current={'middleware': MIDDLEWARE_CLASSES}):
+    """
+    Returns MIDDLEWARE_CLASSES without the middlewares listed in exclude and
+    with the middlewares listed in append.
+
+    The use of a mutable dict is intentional, in order to preserve the state of
+    the MIDDLEWARE_CLASSES tuple across multiple settings files.
+    """
+
+    current['middleware'] = tuple(
+        [m for m in current['middleware'] if m not in exclude]
+    ) + tuple(append)
+    return current['middleware']
+
+def get_apps(exclude=(), append=(), current={'apps': INSTALLED_APPS}):
+    """
+    Returns INSTALLED_APPS without the apps listed in exclude and with the apps
+    listed in append.
+
+    The use of a mutable dict is intentional, in order to preserve the state of
+    the INSTALLED_APPS tuple across multiple settings files.
+    """
+
+    current['apps'] = tuple(
+        [a for a in current['apps'] if a not in exclude]
+    ) + tuple(append)
+    return current['apps']
