44e1b892716b74a3730da92365669f1353eb267e
drkitty <bumblingkitty@gmail.com>
2014-03-13 17:31:18 -0700

    Make 00:00:00:00:00:00 an invalid MAC address

--- cyder/cydhcp/validation.py
+++ cyder/cydhcp/validation.py
@@ -5,5 +5,6 @@ import re
 mac_pattern = re.compile(r'^([0-9a-f]{2}:){5}[0-9a-f]{2}$')
 
 def validate_mac(mac):
-    if not isinstance(mac, basestring) or not mac_pattern.match(mac):
+    if not isinstance(mac, basestring) or not mac_pattern.match(mac) \
+            or mac == '00:00:00:00:00:00':
         raise ValidationError('Invalid MAC address')
