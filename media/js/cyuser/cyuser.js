e5eff47b0a3ad981d2e79ffa8677b18603c2d58d
Josh <joshchurch29@gmail.com>
2014-06-10 11:09:44 -0700

    Small edits to cyuser js

--- media/js/cyuser/cyuser.js
+++ media/js/cyuser/cyuser.js
@@ -1,34 +1,36 @@
 $(document).ready(function() {
-    var formBlock = $('#perm-hidden');
-    var csrfToken = $('#view-metadata').attr('data-csrfToken');
-    $('#id_users').tagsInput({
+    var csrfToken = $('#view-metadata').attr( 'data-csrfToken' );
+
+    $('#id_users').tagsInput( {
         'defaultText': 'add a user',
-        'autocomplete_url': '/core/user/search'});
-    $('#clone-perms-btn, #clone-perms-cancel').click(function(e) {
-        formBlock.slideToggle("slow", function() {
+        'autocomplete_url': '/core/user/search'} );
+
+    $('#clone-perms-btn, #clone-perms-cancel').click( function ( e ) {
+        $('#permissions-form-hidden').slideToggle( "slow", function() {
         });
-    });
-    $('#clone-perms-submit').click(function(e) {
+    } );
+
+    $('#clone-perms-submit').click( function( e ) {
         e.preventDefault();
         postData = {
-            users: ($('#clone-perms-form').find(':input')[0].value),
+            users: $('#clone-perms-form').find( ':input' )[0].value,
             csrfmiddlewaretoken: csrfToken,
         };
-        $.post('/core/user/clone_perms_check/', postData, function(data) {
-            if (data.confirm_str) {
-                var confirmation = confirm(data.confirm_str);
-                if (!confirmation) {
-                    jQuery.each(data.users, function(i, user) {
-                        $('#id_users').removeTag(user);
-                    });
-                };
-            };
-            var url = $('#clone-perms-form').attr('action');
+        $.post( '/core/user/clone_perms_check/', postData, function( data ) {
+            if ( data.confirm_str ) {
+                var confirmation = confirm( data.confirm_str );
+                if ( !confirmation ) {
+                    jQuery.each( data.users, function( i, user ) {
+                        $('#id_users').removeTag( user );
+                    } );
+                }
+            }
+            var url = $('#clone-perms-form').attr( 'action' );
             clone_perms_data = ajax_form_submit(
-                url, $('#clone-perms-form'), csrfToken);
-            if (clone_perms_data.success) {
+                url, $('#clone-perms-form'), csrfToken );
+            if ( clone_perms_data.success ) {
                 location.reload();
-            };
-        }, 'json');
+            }
+        }, 'json' );
     });
 });
