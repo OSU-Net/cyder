3b18434fb57555d5089c9d04f0241f0db356bba6
church29 <joshchurch29@gmail.com>
2014-09-24 13:42:14 -0700

    Rename id's for bug report page

--- media/js/bugreport.js
+++ media/js/bugreport.js
@@ -1,9 +1,9 @@
 $(document).ready( function() {
-    $( document ).on( 'submit', '#Bug-Report', function( e ) {
+    $( document ).on( 'submit', '#js-bugReport', function( e ) {
         e.preventDefault();
-        url = $(location).attr( 'href' );
+        url = $( location ).attr( 'href' );
         var csrfToken = $('#view-metadata').attr('data-csrfToken');
-        var fields = $('#Bug-Report').find( ':input' ).serializeArray();
+        var fields = $('#js-bugReport').find( ':input' ).serializeArray();
         var data = ajax_form_submit( url, fields, csrfToken, function( data ) {
             if ( !data.errors ) {
                 alert('Your bug report was sent successfully. ' +
