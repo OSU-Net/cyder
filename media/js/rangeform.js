5ef990bd4789dc9feb9366577c21aed810d140bd
church29 <joshchurch29@gmail.com>
2014-11-20 11:57:23 -0800

    Refactor rangeform js

--- media/js/rangeform.js
+++ media/js/rangeform.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    var create_btn = document.getElementById('btn create-obj');
-    create_btn.onclick = function() {
-        var network = document.getElementById('id_network');
-        network.onchange = function() {
-            var network_str = (network.options[network.selectedIndex].text).split('/');
-            var initial = (network_str[0].split('.').slice(0, (~~(network_str[1]/8)))).join('.');
-            var start_str = document.getElementById('id_start_str');
-            var end_str = document.getElementById('id_end_str');
-            start_str.value = initial;
-            end_str.value = initial;
-        };
-
-    };
-
+    $( document ).on( 'change', '.networkWizard', function() {
+        var selectedNetwork = $('#id_network').find(':selected');
+        if (selectedNetwork.val() == '') {
+            $('#id_start_str').val('');
+            $('#id_end_str').val('');
+        } else {
+            var networkStr = selectedNetwork.text().split('/')[0];
+            var initial = networkStr.split('.');
+            initial.pop();
+            initial = initial.join('.');
+            $('#id_start_str').val(initial);
+            $('#id_end_str').val(initial);
+        }
+    });
 });
