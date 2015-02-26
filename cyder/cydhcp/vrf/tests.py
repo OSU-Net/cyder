ad65648eeac816b589403c9c14b6d3b167581349
drkitty <bumblingkitty@gmail.com>
2015-02-26 10:46:31 -0800

    Fix VRF test

--- cyder/cydhcp/vrf/tests.py
+++ cyder/cydhcp/vrf/tests.py
@@ -9,5 +9,6 @@ class VrfTests(TestCase, ModelTestMixin):
         return (
             Vrf.objects.create(name='a'),
             Vrf.objects.create(name='bbbbbbbbbbbbb'),
-            Vrf.objects.create(name='Hello, world.'),
+            Vrf.objects.create(name='-c-c-c-'),
+            Vrf.objects.create(name='__d__d__d__'),
         )
