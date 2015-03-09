630024310fd94aa8d9609aea5d529029269728cc
drkitty <bumblingkitty@gmail.com>
2015-03-09 13:41:47 -0700

    Scroll to top of page when opening an object form

--- media/js/views.js
+++ media/js/views.js
@@ -115,6 +115,7 @@ $(document).ready( function() {
                 };
             }
 
+            $(document).scrollTop(0);
             $.ajax({
                 type: 'GET',
                 url: kwargs.get_url,
