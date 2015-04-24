996b4d9758fc0b4c9a25d94cc846539a591cc268
drkitty <bumblingkitty@gmail.com>
2015-04-24 14:47:03 -0700

    Clean up some stuff while I'm at it

--- cyder/cydns/search_utils.py
+++ cyder/cydns/search_utils.py
@@ -50,37 +50,35 @@ def _build_label_domain_queries(label, domain, mx=True, sr=True, tx=True,
     if mx:
         from cyder.cydns.mx.models import MX
         qsets.append(('MX', MX.objects.
-                      filter(**{'label': label, 'domain': domain})))
+                      filter(label=label, domain=domain)))
     if ns:
         if label == '':
             from cyder.cydns.nameserver.models import Nameserver
             qsets.append(('NS', Nameserver.objects.
-                          filter(**{'domain': domain})))
+                          filter(domain=domain)))
     if sr:
         from cyder.cydns.srv.models import SRV
         qsets.append(('SRV', SRV.objects.
-                      filter(**{'label': label, 'domain': domain})))
+                      filter(label=label, domain=domain)))
     if tx:
         from cyder.cydns.txt.models import TXT
         qsets.append(('TXT', TXT.objects.
-                      filter(**{'label': label, 'domain': domain})))
+                      filter(label=label, domain=domain)))
     if ss:
         from cyder.cydns.sshfp.models import SSHFP
         qsets.append(('SSHFP', SSHFP.objects.
-                      filter(**{'label': label, 'domain': domain})))
+                      filter(label=label, domain=domain)))
     if cn:
         from cyder.cydns.cname.models import CNAME
         qsets.append(('CNAME', CNAME.objects.
-                      filter(**{'label': label, 'domain': domain})))
+                      filter(label=label, domain=domain)))
     if ar:
         from cyder.cydns.address_record.models import AddressRecord
-        ars = AddressRecord.objects.filter(
-            **{'label': label, 'domain': domain})
+        ars = AddressRecord.objects.filter(label=label, domain=domain)
         qsets.append(('AddressRecord', ars))
     if intr:
         from cyder.cydhcp.interface.static_intr.models import StaticInterface
-        intrs = StaticInterface.objects.filter(
-            **{'label': label, 'domain': domain})
+        intrs = StaticInterface.objects.filter(label=label, domain=domain)
         qsets.append(('AddressRecord', intrs))
 
     return qsets
