0b0490776bf63f7a2d48d9a1d5f82fa6ef6ada80
Nathan Murrow <nathan.murrow@oregonstate.edu>
2015-09-23 15:41:20 -0700

    add routers fields to network form

--- cyder/cydhcp/network/forms.py
+++ cyder/cydhcp/network/forms.py
@@ -5,10 +5,12 @@ import ipaddr
 
 from cyder.base.constants import IP_TYPES, IP_TYPE_4, IP_TYPE_6
 from cyder.base.eav.forms import get_eav_form
+from cyder.base.eav.models import Attribute
 from cyder.base.mixins import UsabilityFormMixin
 from cyder.cydhcp.network.models import Network, NetworkAV
 from cyder.cydhcp.site.models import Site
 from cyder.cydhcp.vlan.models import Vlan
+from cyder.management.commands.lib.utilities import long2ip
 from cydns.ip.models import ipv6_to_longs
 
 
@@ -20,6 +22,17 @@ class NetworkForm(forms.ModelForm, UsabilityFormMixin):
         help_text="The site the network will be put into. "
                   "Defaults to parent network's site"
     )
+
+    routers = forms.BooleanField(label='Option "routers"', required=False,
+                                 help_text='Auto-create DHCP "routers" option')
+    gateway = forms.CharField(
+        label='Alternate Gateway', required=False,
+        help_text=('Fill in this field to change the default gateway '
+                   'in the "routers" DHCP option.'))
+    subnet_mask = forms.CharField(
+        label='Alternate Subnet Mask', required=False,
+        help_text=('Fill in this field to change the default subnet mask '
+                   'in the "routers" DHCP option.'))
 
     def __init__(self, *args, **kwargs):
         super(NetworkForm, self).__init__(*args, **kwargs)
@@ -49,11 +62,50 @@ class NetworkForm(forms.ModelForm, UsabilityFormMixin):
                 network = ipaddr.IPv6Network(network_str)
                 ip_upper, ip_lower = ipv6_to_longs(network.network)
 
+            if cleaned_data["routers"]:
+                for key in ["gateway", "subnet_mask"]:
+                    value = cleaned_data.get(key)
+                    if key == "subnet_mask" and "/" in value:
+                        if ip_type == IP_TYPE_4:
+                            size = 32
+                        else:
+                            size = 128
+                        _, mask = tuple(value.split("/"))
+                        mask = (1 << (size-int(mask))) - 1
+                        base = (1 << size) - 1
+                        value = long2ip(base ^ mask)
+                    if value:
+                        if ip_type == IP_TYPE_4:
+                            value = str(ipaddr.IPv4Network(value))
+                        elif ip_type == IP_TYPE_6:
+                            value = str(ipaddr.IPv6Network(value))
+                        value, _ = tuple(value.split("/"))
+                        cleaned_data[key] = value
+
         except ipaddr.AddressValueError, e:
             raise ValidationError("Bad IP address {0}".format(e))
         except ipaddr.NetmaskValueError, e:
             raise ValidationError("Bad netmask {0}".format(e))
         return cleaned_data
+
+    def save(self, *args, **kwargs):
+        network = super(NetworkForm, self).save(*args, **kwargs)
+        if self.cleaned_data['routers']:
+            attr_routers = Attribute.objects.get(name="routers")
+            attr_subnet_mask = Attribute.objects.get(name="subnet-mask")
+            if not self.cleaned_data.get('gateway'):
+                gateway, _ = tuple(network.network_str.split("/"))
+            else:
+                gateway = self.cleaned_data['gateway']
+            if not self.cleaned_data.get('subnet_mask'):
+                subnet_mask = str(network.network.netmask)
+            else:
+                subnet_mask = self.cleaned_data['subnet_mask']
+            NetworkAV.objects.create(
+                entity=network, attribute=attr_routers, value=gateway)
+            NetworkAV.objects.create(
+                entity=network, attribute=attr_subnet_mask, value=subnet_mask)
+        return network
 
 
 NetworkAVForm = get_eav_form(NetworkAV, Network)
